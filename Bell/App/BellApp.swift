7c366b93be2a4f3e057a806dc9196cf790c2415b
briantraudt <briantraudt@gmail.com>
2026-08-04 16:04:41 -0500

    Add first-run onboarding app state

--- Bell/App/BellApp.swift
+++ Bell/App/BellApp.swift
@@ -6,9 +6,15 @@ struct BellApp: App {
 
     var body: some Scene {
         WindowGroup {
-            RootView()
-                .environment(appState)
-                .preferredColorScheme(.light)
+            Group {
+                if appState.hasCompletedOnboarding {
+                    RootView()
+                } else {
+                    OnboardingFlowView()
+                }
+            }
+            .environment(appState)
+            .preferredColorScheme(.light)
         }
     }
 }
@@ -22,6 +28,34 @@ final class AppState {
     var bookings = Booking.demo
     var reminders = Reminder.demo
     var family = FamilyMember.demo
+    var hasCompletedOnboarding: Bool {
+        didSet {
+            UserDefaults.standard.set(hasCompletedOnboarding, forKey: "bell.onboarding.completed")
+        }
+    }
+    var textScale: Double {
+        didSet {
+            UserDefaults.standard.set(textScale, forKey: "bell.text.scale")
+        }
+    }
+
+    init() {
+        hasCompletedOnboarding = UserDefaults.standard.bool(forKey: "bell.onboarding.completed")
+        let savedScale = UserDefaults.standard.double(forKey: "bell.text.scale")
+        textScale = savedScale == 0 ? 1.0 : savedScale
+    }
+
+    func completeOnboarding(firstName: String, lastName: String, phone: String, city: String, readAloud: Bool) {
+        profile = UserProfile(id: UUID(), firstName: firstName, lastName: lastName, phone: phone, city: city, readAloud: readAloud)
+        bookings = []
+        reminders = []
+        family = []
+        hasCompletedOnboarding = true
+    }
+
+    func resetForTesting() {
+        hasCompletedOnboarding = false
+    }
 }
 
 enum BellTab: Hashable { case home, plans, me }
